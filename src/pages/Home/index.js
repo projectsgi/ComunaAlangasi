2263aae7efdc1e66a18559e8e9170e47b9ef0778
Genesis <geneanchundia87@gmail.com>
2024-03-25 17:19:25 -0500

    Cambio1

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -11,7 +11,7 @@ const sliderContent = [
     id: "image01",
     image: "alangasi1.jpg",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "Alangasí, tierra de arte, cultura, naturaleza y buena comida. Esta parroquia rural ubicada en el Valle de Los Chillos, al suroriente de Quito, es un tesoro que aloja a artistas, artesanos y muy buena comida.",
   },
   {
     id: "image02",
